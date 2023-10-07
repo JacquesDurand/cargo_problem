6207e9b5811a6007329a9295cf85d152485f84c4
Hugo Nicolas <hugo.nicolas@les-tilleuls.coop>
2023-10-07 13:27:31 +0200

    chore: dir architectory

---
+++ cargo/public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Shared\Infra\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
